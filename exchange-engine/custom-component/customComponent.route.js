bdb9beffaf22e16ca604a1d1456314c9f39c010c
abhijeetdrapcode <abhijeet@drapcode.com>
2025-05-08 17:02:32 +0530

    deleted files

--- exchange-engine/custom-component/customComponent.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getCustomComponentConfig } from './customComponent.controller';
-
-const customComponentRoute = express.Router();
-
-customComponentRoute.get('/:uuid', getCustomComponentConfig);
-
-export default customComponentRoute;
