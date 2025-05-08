caec7918dfeb8a414b1b559d0717ccfbd7b7e05e
abhijeetdrapcode <abhijeet@drapcode.com>
2025-05-08 16:55:03 +0530

    deleted files

--- exchange-engine/langchain-polyfill.js
+++
@@ -1,4 +0,0 @@
-require('web-streams-polyfill/dist/polyfill.js');
-require('core-js/modules/web.structured-clone.js');
-const fetch = require('node-fetch');
-if (!globalThis.fetch) globalThis.fetch = fetch;
--- exchange-engine/middleware/dbchecking.middleware.js
+++
@@ -1,7 +0,0 @@
-const DBAuthCheck = (req, res, next) => {
-  const { db } = req;
-  req.db = db || null;
-  next();
-};
-
-export default DBAuthCheck;
