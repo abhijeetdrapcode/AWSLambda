64dbc08a3c7938bb0035f9caa224a98d780d1d02
abhijeetdrapcode <abhijeet@drapcode.com>
2025-05-14 13:01:26 +0530

    deleted files

--- start-surface.sh
+++
@@ -1,9 +0,0 @@
-FOLDER=exchange-surface
-echo "******* Making Build *************"
-cd $FOLDER
-npm install
-echo "**** Make Build ****"
-npm run publish
-
-pm2 restart ecosystem.config.js
-pm2 logs exchange-surface
