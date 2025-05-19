ab9ca6309372767d5d87e385a3d75e927cb48e21
abhijeetdrapcode <abhijeet@drapcode.com>
2025-05-19 16:00:53 +0530

    Deleted some files

--- abhijeetproject1832/exchange-engine/collection/collection.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { checkUniqueValidation, findById, findByName } from './collection.controller';
-
-const collectionRouter = express.Router();
-collectionRouter.get('/:collectionName/name', findByName);
-collectionRouter.get('/:uuid/id', findById);
-collectionRouter.get('/:collectionName/items/unique-check', checkUniqueValidation);
-export default collectionRouter;
--- abhijeetproject1832/exchange-engine/profiling/profiler.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { interceptLogger } from 'drapcode-utility';
-import { deleteProfiler } from './profiler.controller';
-const profilerRoute = express.Router();
-
-profilerRoute.post('/delete', interceptLogger, deleteProfiler);
-
-export default profilerRoute;
